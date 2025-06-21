958bb681d4661d7d2396a072fb36af1d1c292b79
alexey.zh <mailbox.stdio@gmail.com>
2025-06-21 08:50:12 +0500

    ci: update k8s manifests

---
+++ test/integration/k8s/01-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+kubectl apply -f manifests/
+kubectl -n pgrwl-test rollout restart sts postgres
+kubectl -n pgrwl-test rollout restart sts prometheus
+kubectl -n pgrwl-test rollout restart sts loki
+kubectl -n pgrwl-test rollout restart ds promtail
+kubectl -n pgrwl-test rollout restart ds node-exporter
+kubectl -n pgrwl-test rollout restart deploy grafana
