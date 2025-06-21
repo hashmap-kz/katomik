aaf744e743854cd00b5642c542a9ef2770824c0b
alexey.zh <mailbox.stdio@gmail.com>
2025-06-21 17:46:13 +0500

    ci: simplify manifests for tests

--- test/integration/k8s/01-deploy.sh
+++ test/integration/k8s/01-deploy.sh
@@ -4,9 +4,4 @@ set -euo pipefail
 kubectl apply -f manifests/
 kubectl -n pgrwl-test rollout restart sts postgres
 kubectl -n pgrwl-test rollout restart sts prometheus
-kubectl -n pgrwl-test rollout restart sts loki
-kubectl -n pgrwl-test rollout restart ds promtail
-kubectl -n pgrwl-test rollout restart ds node-exporter
 kubectl -n pgrwl-test rollout restart deploy grafana
-kubectl -n pgrwl-test rollout restart deploy kube-state-metrics
-kubectl -n pgrwl-test rollout restart deploy alertmanager
